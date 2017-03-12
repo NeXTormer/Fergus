0395868623cb85bf8543ca316d24a512a102aba2
Felix Holz <felix.holz@me.com>
2017-03-12 16:07:54 +0100

    Initial Commit

---
+++ Fergus-core/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <GLFW\glfw3.h>
+
+int main()
+{
+	std::cout << "Rendl" << std::endl;
+	if (glfwInit())
+	{
+		std::cout << "peter" << std::endl;
+
+	}
+
+	
+	system("pause");
+	return 0;
+}
