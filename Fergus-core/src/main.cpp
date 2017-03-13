0c3c4667497fe6bb7d8b79c06e326125c5b2612f
Felix Holz <felix.holz@me.com>
2017-03-13 22:48:15 +0100

    added glm

--- Fergus-core/src/main.cpp
+++ Fergus-core/src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 #include "graphics\window.h"
+#include "glm/glm.hpp"
 
 int main(void)
 {
+
+	glm::vec2 peter(1, 2);
+	std::cout << peter.x << std::endl;
 	
 	Window window(800, 600, "Lex ist lustig!");
 	glClearColor(0.6, 1, 0.5, 1);
