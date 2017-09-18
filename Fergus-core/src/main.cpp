8ef8ef0fe2571901e691999bd505ba312009fde0
Felix Holz <felix.holz@me.com>
2017-09-18 14:18:08 +0200

    teapot pfuscherei

--- Fergus-core/src/main.cpp
+++ Fergus-core/src/main.cpp
@@ -19,6 +19,10 @@
 #include "graphics\models\texturedmodel.h"
 #include "graphics\entities\entity.h"
 #include "graphics\models\model.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
 
 //TODO: load shader in Mesh::draw
 
@@ -143,7 +147,7 @@ int main()
 
 	//Entity entity(&texturedModel, glm::vec3(0, 0, -2), glm::vec3(0, 0, 0), 1);
 	
-	Model model("res/models/nanosuit/nanosuit.dae");
+	Model suit("res/models/teapot.obj");
 	
 
 	while (!window.closed())
@@ -153,8 +157,23 @@ int main()
 		camera.update();
 
 		shader.enable();
-		model.draw(&shader);
 
+		// view/projection transformations
+		
+		glm::mat4 projection = glm::perspective(90.0f, 16.0f / 9.0f, 0.1f, 100.0f);
+		glm::mat4 view = camera.getViewMatrix();
+		shader.setUniformMat4("pr_matrix", projection);
+		shader.setUniformMat4("vw_matrix", view);
+
+		// render the loaded model
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0, -0.1, 0));
+		model = glm::scale(model, glm::vec3(0.002, 0.002, 0.002));
+		model = glm::rotate(model, 100.f, glm::vec3(0, 1, 0));
+
+		shader.setUniformMat4("ml_matrix", model);
+		
+		suit.draw(&shader);
 
 
 		//entity.rotate(glm::vec3(0.01f, 0.01f, 0.0f));
